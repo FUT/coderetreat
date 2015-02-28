758d36c9af1d121124cf30efa850d1548a63fd90
Yury Kaliada <fut.wrk@gmail.com>
2015-02-28 12:04:22 +0300

    test 1
    Signed-off-by: Yury Kaliada <fut.wrk@gmail.com>

---
+++ life.rb
@@ -0,0 +1,51 @@
+require "minitest/autorun"
+
+class TestLife < Minitest::Test
+  describe 'dead cell' do
+    def setup
+      @cell = Cell.new 1, 2, Cell::DEAD
+    end
+
+    describe '#alive?' do
+      it 'should return false for alive cell' do
+        @cell.alive?.must_equal false
+      end
+    end
+
+    describe '#dead?' do
+      it 'should return true for dead cell' do
+        @cell.dead?.must_equal true
+      end
+    end
+
+    describe '#x' do
+      it 'should return x coordinate' do
+        @cell.x.must_equal 1
+      end
+    end
+
+    describe '#y' do
+      it 'should return x coordinate' do
+        @cell.x.must_equal 1
+      end
+    end
+  end
+
+  describe 'alive cell' do
+    def setup
+      @cell = Cell.new 3, 4, Cell::ALIVE
+    end
+
+    describe '#alive?' do
+      it 'should return true for alive cell' do
+        @cell.alive?.must_equal true
+      end
+    end
+
+    describe '#dead?' do
+      it 'should return false for alive cell' do
+        @cell.dead?.must_equal false
+      end
+    end
+  end
+end
