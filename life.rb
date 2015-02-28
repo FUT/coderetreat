366922a6c0d318aeed7b2eb71e40993c354a76d1
anatoliliotych <anatoli@rightscale.com>
2015-02-28 13:50:19 +0300

    3

--- life.rb
+++ life.rb
@@ -22,6 +22,11 @@ RSpec.describe "LIFE" do
         expect(cell.next_state).to eq(false)
       end
 
+      it "should have #reborn!" do
+        cell.next_state = false
+        cell.reborn!
+        expect(cell.state).to eq(false)
+      end
 
     end
   end
